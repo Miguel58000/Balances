70e2fd0403f1889f866d181a91d3c821d447c9c2
Miguel58000 <miguelrodriguezips36@gmail.com>
2026-04-29 14:30:09 -0300

    Security fix

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtF0atf1iH8zWPYdxtgKkJt7KH9dzrBpc",
-  authDomain: "balances-64616.firebaseapp.com",
-  projectId: "balances-64616",
-  storageBucket: "balances-64616.firebasestorage.app",
-  messagingSenderId: "891046122820",
-  appId: "1:891046122820:web:b99604c69597faee5d3311"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
